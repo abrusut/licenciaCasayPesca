9cd25b9bd6a02b46e46b9e1ab88999d4f459d998
abrusut <andresbrusutti@gmail.com>
2019-03-15 08:59:40 -0300

    aplicacion con 3.4 y bundle de css materialize de stg

---
+++ src/Form/TecnicoType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TecnicoType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('especialidad')
+            ->add('nombre')
+            ->add('apellido')
+            ->add('dni')
+            ->add('email')
+            ->add('telefono')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MProd\LicenciaCyPBundle\Entity\Tecnico',
+            'csrf_protection' => TRUE,
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'mprod_licenciacypbundle_tecnico';
+    }
+}
